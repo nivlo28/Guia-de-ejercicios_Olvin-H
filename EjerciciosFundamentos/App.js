90f013d42de65f1b2c03bf070f6d3f9d26830723
nivlo28 <efrenhilton1980@gmail.com>
2026-05-08 17:04:20 -0600

    a y b
    Se creo la variable nombre y la variable de edad

---
+++ EjerciciosFundamentos/App.js
@@ -0,0 +1,37 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+
+const name="Olvin";
+  const edad=18;
+
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.texto}>
+         Como estas mi amorcito  {name}
+         </Text>
+
+       <Text style={styles.texto}>
+        {edad>=18 ? "Mayor de edad" : "Menor de edad"}
+      </Text>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#d86a6ad0',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  texto:{
+    fontSize:20,
+    color:'white',
+    margin:10,
+  },
+});
