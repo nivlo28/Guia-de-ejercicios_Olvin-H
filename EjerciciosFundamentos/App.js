9a8f1cc0c92cc248561f1310f510ee7dd840883b
nivlo28 <efrenhilton1980@gmail.com>
2026-05-09 09:21:04 -0600

    c
    -agregue lo del arreglo .map()

--- EjerciciosFundamentos/App.js
+++ EjerciciosFundamentos/App.js
@@ -5,21 +5,31 @@ export default function App() {
 
 const name="Olvin";
   const edad=18;
+  const productos=[{ id: 1, nombre: "Laptop" },
+                  { id: 2, nombre: "Mouse" },
+                  { id: 3, nombre: "Teclado" }]
+
 
 
   return (
     <View style={styles.container}>
       <Text style={styles.texto}>
-         Como estas mi amorcito  {name}
+         Como estas {name}
          </Text>
 
        <Text style={styles.texto}>
         {edad>=18 ? "Mayor de edad" : "Menor de edad"}
       </Text>
 
+       {productos.map((producto) =>(
+      <Text style={styles.texto}>
+        {productos.name}
+      </Text>
+       ))};
+
       <StatusBar style="auto" />
     </View>
-  );
+       );
 }
 
 const styles = StyleSheet.create({
